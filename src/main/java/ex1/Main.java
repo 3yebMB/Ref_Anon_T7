ec695ba1ba2b3d6a97b4aba321241980eecde3d8
Michael <Zuev>
2019-02-21 14:30:45 +0500

    created annotations

--- src/main/java/ex1/Main.java
+++ src/main/java/ex1/Main.java
@@ -1,4 +1,22 @@
 package ex1;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void method1() {
+
+    }
+
+    @BeforeSuite
+    public void method2() {
+
+    }
+
+    @AfterSuite
+    public void method3() {
+
+    }
 }
