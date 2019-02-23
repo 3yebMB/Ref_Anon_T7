c40d3963b398bd7090916c029ffdaa4b3f8b6fc5
3yebmb <3yebmb@gmail.com>
2019-02-23 11:49:47 +0500

    Changed logic of passing test

--- src/main/java/ex1/Main.java
+++ src/main/java/ex1/Main.java
@@ -2,21 +2,6 @@ package ex1;
 
 public class Main {
     public static void main(String[] args) {
-
-    }
-
-    @Test
-    public void method1() {
-
-    }
-
-    @BeforeSuite
-    public void method2() {
-
-    }
-
-    @AfterSuite
-    public void method3() {
-
+        TestingClass.start(TestingClass.class);
     }
 }
