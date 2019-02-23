d165911e6f70fc4acedda54e9b83865221850153
3yebmb <3yebmb@gmail.com>
2019-02-23 11:55:03 +0500

    Created testing method

---
+++ src/main/java/ex1/TestingClass.java
@@ -0,0 +1,78 @@
+package ex1;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class TestingClass {
+
+    public static void start(Class cls) {
+        performTests(cls);
+    }
+
+    private static void performTests(Class cls) throws RuntimeException {
+        TestingClass testingObj = null;
+        try {
+            testingObj = (TestingClass)cls.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        Method beforeMethod = null;
+        Method afterMethod = null;
+        Method[] methods = cls.getMethods();
+        List<MethodsPriority> testingMethods = new ArrayList<>();
+
+        for (Method method : methods)
+            if (method.getAnnotation(BeforeSuite.class) != null) {
+                if (beforeMethod != null)
+                    throw new RuntimeException("The method with the @BeforeSuite annotation should be one");
+                beforeMethod = method;
+            } else if (method.getAnnotation(AfterSuite.class) != null) {
+                if (afterMethod != null)
+                    throw new RuntimeException("The method with the @AfterSuite annotation should be one");
+                afterMethod = method;
+            } else if (method.getAnnotation(Test.class) != null){
+                Test annotationTst = method.getAnnotation(Test.class);
+                testingMethods.add(new MethodsPriority(method, annotationTst.value()));
+            }
+
+        testingMethods.sort(
+                Comparator.comparing(MethodsPriority::getPriority));
+
+        try {
+            if (beforeMethod != null)
+                beforeMethod.invoke(testingObj);
+
+            for (MethodsPriority methodWP : testingMethods)
+                methodWP.method.invoke(testingObj);
+
+            if (afterMethod != null)
+                afterMethod.invoke(testingObj);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test(value = 5)
+    public void Test5() {
+        System.out.println("Performed Test 5");
+    }
+    @Test(value = 3)
+    public void Test2(){
+        System.out.println("Performed Test 2");
+    }
+    @Test(value = 1)
+    public void Test1(){
+        System.out.println("Performed Test 1");
+    }
+    @BeforeSuite
+    public void BeforeAll(){
+        System.out.println("This method is performed before all");
+    }
+    @AfterSuite
+    public void AfterAll() {
+        System.out.println("This method is performed after all");
+    }
+}
